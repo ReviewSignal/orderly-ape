6663a3f81cb67745324bb8379ccb7579540cd35f
Calin Don <calin.don@gmail.com>
2024-09-16 14:43:37 +0300

    feat: Record test completion time

--- webapp/loadtest/signals.py
+++ webapp/loadtest/signals.py
@@ -4,21 +4,41 @@ from django.utils import timezone
 
 from .models import TestRunLocation
 
+# Delay before starting the test run after all locations are ready so that the
+# locations have time to pick-up the start time and begin testing at the same time.
+TEST_RUN_START_DELAY_SECONDS = 30
 
-@receiver(post_save, sender=TestRunLocation, dispatch_uid="set_start_test_at")
-def set_start_test_at(sender, instance: TestRunLocation, created, **kwargs):
+
+@receiver(post_save, sender=TestRunLocation, dispatch_uid="set_test_started_at")
+def set_test_started_at(sender, instance: TestRunLocation, created, **kwargs):
     test_run = instance.test_run
     if test_run is None:
         return
 
     if (
         instance.status == TestRunLocation.Status.READY
-        and test_run.start_test_at is None
+        and test_run.started_at is None
         and test_run.locations.exclude(status=TestRunLocation.Status.READY).count() == 0
     ):
-        test_run.start_test_at = (
-            timezone.now() + timezone.timedelta(seconds=20)
+        test_run.started_at = (
+            timezone.now() + timezone.timedelta(seconds=TEST_RUN_START_DELAY_SECONDS)
         ).replace(microsecond=0)
+        test_run.save()
+
+
+@receiver(post_save, sender=TestRunLocation, dispatch_uid="set_test_completed_at")
+def set_test_completed_at(sender, instance: TestRunLocation, created, **kwargs):
+    test_run = instance.test_run
+    if test_run is None:
+        return
+
+    if (
+        instance.status == TestRunLocation.Status.COMPLETED
+        and test_run.completed_at is None
+        and test_run.locations.exclude(status=TestRunLocation.Status.COMPLETED).count()
+        == 0
+    ):
+        test_run.completed_at = (timezone.now()).replace(microsecond=0)
         test_run.save()
 
 
